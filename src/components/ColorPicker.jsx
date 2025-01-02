c8096569e88693703113c9611e6f72f4d456edaa
Spac3G0at <victor.s.marie@gmail.com>
2025-01-02 10:02:23 +0100

    color picker & updated mock

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect, useLayoutEffect } from "react";
+import styled from "styled-components";
 
 const ColorPicker = ({ baseColor = "#ffffff", onChange }) => {
   const [color, setColor] = useState(baseColor); // Initial color is white
@@ -10,7 +11,7 @@ const ColorPicker = ({ baseColor = "#ffffff", onChange }) => {
 
   // Handle color change
   const handleInputChange = (event) => {
-    setColor(event.target.value); // Set the new color as user selects it
+    setColor(event.target.value);
   };
 
   // Open the color input dialog when the div is clicked
@@ -18,6 +19,7 @@ const ColorPicker = ({ baseColor = "#ffffff", onChange }) => {
     colorInputRef.current.click(); // Trigger the color input dialog
   };
 
+  // Update the color on blur
   useLayoutEffect(() => {
     if (!colorInputRef.current) return;
     const logEvent = (e) => {
@@ -31,37 +33,36 @@ const ColorPicker = ({ baseColor = "#ffffff", onChange }) => {
   }, [colorInputRef, onChange]);
 
   return (
-    <div
-      style={{
-        fontFamily: "Arial, sans-serif",
-        textAlign: "center",
-        position: "relative", // Set the container to relative to position the popover
-      }}
-    >
-      {/* Display the selected color as a clickable div */}
-      <div
-        onClick={handleDivClick} // Handle click on the div to trigger color picker
-        style={{
-          width: "26px",
-          height: "20px",
-          border: "1px solid white", // White border
-          backgroundColor: color, // Background color set to the selected color
-          cursor: "pointer", // Pointer cursor to indicate it's clickable
-        }}
-      ></div>
+    <Root>
+      <Display $color={color} onClick={handleDivClick}></Display>
 
       {/* Hidden color input (will trigger the picker when the div is clicked) */}
-      <input
+      <Picker
         ref={colorInputRef}
         type="color"
         value={color}
-        onChange={handleInputChange} // Update color when picked
-        style={{
-          display: "none", // Hide the input element (the square box)
-        }}
+        onChange={handleInputChange}
       />
-    </div>
+    </Root>
   );
 };
 
 export default ColorPicker;
+
+const Root = styled.div`
+  font-family: Arial, sans-serif;
+  text-align: center;
+  position: relative;
+`;
+
+const Display = styled.div`
+  width: 26px;
+  height: 20px;
+  border: 1px solid white;
+  cursor: pointer;
+  background-color: ${({ $color }) => $color};
+`;
+
+const Picker = styled.input`
+  display: none;
+`;
