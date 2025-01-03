3bd48441477d985e28375cba211263533b752245
Spac3G0at <victor.s.marie@gmail.com>
2025-01-03 22:36:18 +0100

    resumes grid

--- src/pages/ResumesPage.jsx
+++ src/pages/ResumesPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import PageLoader from "./PageLoader";
 import ResumeItem from "../components/resume/ResumeItem";
 import Card from "../components/Card";
+import styled from "styled-components";
 
 const mock = [
   {
@@ -32,13 +33,20 @@ const ResumesPage = () => {
   return (
     <div>
       {loading && <PageLoader />}
-      {resumes.map((resume) => (
-        <Card key={resume.id}>
-          <ResumeItem resume={resume} large />
-        </Card>
-      ))}
+      <Content>
+        {resumes.map((resume) => (
+          <Card key={resume.id}>
+            <ResumeItem resume={resume} large />
+          </Card>
+        ))}
+      </Content>
     </div>
   );
 };
 
 export default ResumesPage;
+
+const Content = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+`;
